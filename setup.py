d4eef750fa55f584522edc8df6f1101fc1afcdb3
Densaugeo <a@b.c>
2021-01-01 19:21:27 -0500

    Update to accomodate changes in Python 3.7-8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='uploadserver',
-    version='0.1.2',
+    version='0.2.0',
     author='Densaugeo',
     author_email='author@example.com',
     description='Python\'s http.server extended to include a file upload page',
--- uploadserver/__main__.py
+++ uploadserver/__main__.py
@@ -1,20 +1,54 @@
-import http.server, argparse, uploadserver
+import http.server, sys, argparse, uploadserver
+
+if sys.version_info.major >= 3 and sys.version_info.minor >= 7:
+    import os, functools
+
+if sys.version_info.major >= 3 and sys.version_info.minor >= 8:
+    import contextlib
 
 if __name__ == '__main__':
+    # In Python 3.8, http.server.test() was altered to use None instead of '' as the default for its bind parameter
+    if sys.version_info.major <= 3 and sys.version_info.minor < 8:
+        bind_default = ''
+    else:
+        bind_default = None
+    
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cgi', action='store_true',
-                       help='Run as CGI Server')
-    parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
-                        help='Specify alternate bind address '
-                             '[default: all interfaces]')
-    parser.add_argument('port', action='store',
-                        default=8000, type=int,
-                        nargs='?',
-                        help='Specify alternate port [default: 8000]')
+    parser.add_argument('--cgi', action='store_true', help='Run as CGI Server')
+    parser.add_argument('--bind', '-b', default=bind_default, metavar='ADDRESS',
+        help='Specify alternate bind address [default: all interfaces]')
+    parser.add_argument('port', action='store', default=8000, type=int, nargs='?',
+        help='Specify alternate port [default: 8000]')
+    # Directory option was added to http.server in Python 3.7
+    if sys.version_info.major >= 3 and sys.version_info.minor >= 7:
+        parser.add_argument('--directory', '-d', default=os.getcwd(),
+            help='Specify alternative directory [default:current directory]')
     args = parser.parse_args()
+    
     if args.cgi:
         handler_class = uploadserver.CGIHTTPRequestHandler
+    elif sys.version_info.major >= 3 and sys.version_info.minor >= 7:
+        handler_class = functools.partial(uploadserver.SimpleHTTPRequestHandler, directory=args.directory)
     else:
         handler_class = uploadserver.SimpleHTTPRequestHandler
+    
+    # This was added to http.server's main section in Python 3.8
+    if sys.version_info.major <= 3 and sys.version_info.minor < 8:
+        server_class = http.server.ThreadingHTTPServer
+    else:
+        class DualStackServer(http.server.ThreadingHTTPServer):
+            def server_bind(self):
+                # suppress exception when protocol is IPv4
+                with contextlib.suppress(Exception):
+                    self.socket.setsockopt(
+                        socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+                return super().server_bind()
+        server_class = DualStackServer
+    
     print('File upload available at /upload')
-    http.server.test(HandlerClass=handler_class, port=args.port, bind=args.bind)
+    http.server.test(
+        HandlerClass=handler_class,
+        ServerClass=server_class,
+        port=args.port,
+        bind=args.bind,
+    )
