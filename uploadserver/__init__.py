283d1cabff5af315c27c53a677f63c8c336f6dce
NteRySin <88823141+NteRySin@users.noreply.github.com>
2021-08-16 11:03:00 +0200

    Improvements

--- uploadserver/__init__.py
+++ uploadserver/__init__.py
@@ -32,37 +32,43 @@ Token (only needed if server was started with token option): <input name="token"
 
 def send_upload_page(handler):
     handler.send_response(http.HTTPStatus.OK)
-    handler.send_header("Content-Type", 'text/html; charset=utf-8')
-    handler.send_header("Content-Length", len(upload_page))
+    handler.send_header('Content-Type', 'text/html; charset=utf-8')
+    handler.send_header('Content-Length', len(upload_page))
     handler.end_headers()
     handler.wfile.write(upload_page)
 
 def receive_upload(handler):
-    result = 0
+    result = (http.HTTPStatus.INTERNAL_SERVER_ERROR, 'Server error')
     
     form = cgi.FieldStorage(fp=handler.rfile, headers=handler.headers, environ={'REQUEST_METHOD': 'POST'})
-    if 'files' not in form: return
-    fields = form['files']
+    if 'files' not in form:
+        return (http.HTTPStatus.BAD_REQUEST, 'Field "files" not found')
+    
     if not isinstance(fields, list): fields = [fields]
+    upload_directory_path = pathlib.Path.(DIRECTORY)
     
     for field in fields:
         if field.file and field.filename:
-            filename = pathlib.Path(field.filename).name
+            file_path = upload_directory / field.filename
         else:
-            filename = None
-    
+            return (http.HTTPStatus.BAD_REQUEST, 'Field "filename" not found')
+        
+        if not file_path.resolve().is_relative_to(upload_directory):
+            handler.log_message('Path traversal attempt: {}'.format(field.filename))
+            return (http.HTTPStatus.BAD_REQUEST, 'Path traversal attempt')
+        
         if TOKEN:
             # server started with token.
             if 'token' not in form or form['token'].value != TOKEN:
                 # no token or token error
                 handler.log_message('Upload of "{}" rejected (bad token)'.format(filename))
-                result = http.HTTPStatus.FORBIDDEN
+                result = (http.HTTPStatus.FORBIDDEN, 'Token is enabled on this server, and your token is wrong')
                 continue # continue so if a multiple file upload is rejected, each file will be logged
         
-        if filename:
-            with open(pathlib.Path(DIRECTORY) / filename, 'wb') as f:
-                f.write(field.file.read())
-                handler.log_message('Upload of "{}" accepted'.format(filename))
+        with open(file_path, 'wb') as f:
+            f.write(field.file.read())
+            handler.log_message('Upload of "{}" accepted'.format(file_path.name))
+            result = (http.HTTPStatus.NO_CONTENT, None)
     
     return result
 
@@ -73,13 +79,14 @@ class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     
     def do_POST(self):
         if self.path == '/upload':
-            retcode = receive_upload(self)
-            if http.HTTPStatus.FORBIDDEN == retcode:
-                self.send_error(retcode, "Token is enabled on this server, and your token is error")
-                return
-            self.send_response(http.HTTPStatus.NO_CONTENT)
-            self.end_headers()
-        else: self.send_error(http.HTTPStatus.NOT_FOUND, "Can only POST to /upload")
+            result = receive_upload(self)
+            if result.first < http.HTTPStatus.BAD_REQUEST:
+                self.send_response(result.first, result.second)
+                self.end_headers()
+            else 
+                self.send_error(result.first, result.second)
+        else
+          self.send_error(http.HTTPStatus.BAD_REQUEST, 'Can only POST to /upload')
 
 class CGIHTTPRequestHandler(http.server.CGIHTTPRequestHandler):
     def do_GET(self):
@@ -88,10 +95,11 @@ class CGIHTTPRequestHandler(http.server.CGIHTTPRequestHandler):
     
     def do_POST(self):
         if self.path == '/upload':
-            retcode = receive_upload(self)
-            if http.HTTPStatus.FORBIDDEN == retcode:
-                self.send_error(retcode, "Token is enabled on this server, and your token is error")
-                return
-            self.send_response(http.HTTPStatus.NO_CONTENT)
-            self.end_headers()
-        else: http.server.CGIHTTPRequestHandler.do_POST(self)
+            result = receive_upload(self)
+            if result.first < http.HTTPStatus.BAD_REQUEST:
+                self.send_response(result.first, result.second)
+                self.end_headers()
+            else 
+                self.send_error(result.first, result.second)
+        else
+          self.send_error(http.HTTPStatus.BAD_REQUEST, 'Can only POST to /upload') # TODO: Check if it corresponds to the previous implementation
