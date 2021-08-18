16544d94f0a108ce4b574c0e2cb296654b7a2a25
Densaugeo <>
2021-08-18 00:21:09 -0400

    Refactor to support NteRySin's extension

--- uploadserver/__init__.py
+++ uploadserver/__init__.py
@@ -1,7 +1,12 @@
-import http.server, http, cgi, pathlib
+import http.server, http, cgi, pathlib, sys, argparse, ssl, os
 
+if sys.version_info.major > 3 or sys.version_info.minor >= 7:
+    import functools
 
-upload_page = bytes('''<!DOCTYPE html>
+if sys.version_info.major > 3 or sys.version_info.minor >= 8:
+    import contextlib
+
+UPLOAD_PAGE = bytes('''<!DOCTYPE html>
 <html>
 <head>
 <title>File Upload</title>
@@ -32,9 +37,9 @@ Token (only needed if server was started with token option): <input name="token"
 def send_upload_page(handler):
     handler.send_response(http.HTTPStatus.OK)
     handler.send_header('Content-Type', 'text/html; charset=utf-8')
-    handler.send_header('Content-Length', len(upload_page))
+    handler.send_header('Content-Length', len(UPLOAD_PAGE))
     handler.end_headers()
-    handler.wfile.write(upload_page)
+    handler.wfile.write(UPLOAD_PAGE)
 
 def receive_upload(handler):
     result = (http.HTTPStatus.INTERNAL_SERVER_ERROR, 'Server error')
@@ -100,3 +105,164 @@ class CGIHTTPRequestHandler(http.server.CGIHTTPRequestHandler):
                 self.send_error(result[0], result[1])
         else:
             http.server.CGIHTTPRequestHandler.do_POST(self)
+
+def ssl_wrap(socket):
+    server_root = pathlib.Path(args.directory).resolve()
+    
+    if server_root in pathlib.Path(args.server_certificate).resolve().parents:
+        print('Server certificate \'{}\' is inside web server root \'{}\', exiting'.format(
+            args.server_certificate, server_root))
+        sys.exit(3)
+    if args.client_certificate:
+        if server_root in pathlib.Path(args.client_certificate).resolve().parents:
+            print('Client certificate \'{}\' is inside web server root \'{}\', exiting'.format(
+                args.client_certificate, server_root))
+            sys.exit(3)
+    
+    try:
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.load_cert_chain(certfile=args.server_certificate)
+    except FileNotFoundError:
+        print('Server certificate \'{}\' not found, exiting'.format(args.server_certificate))
+        sys.exit(4)
+    
+    try:
+        if args.client_certificate:
+            context.load_verify_locations(cafile=args.client_certificate)
+            context.verify_mode = ssl.CERT_REQUIRED
+    except FileNotFoundError:
+        print('Client certificate \'{}\' not found, exiting'.format(args.client_certificate))
+        sys.exit(4)
+    
+    try:
+        return context.wrap_socket(socket, server_side=True)
+    except ssl.SSLError as e:
+        print('SSL error: {}, exiting'.format(e))
+        sys.exit(5)
+
+if sys.version_info.major == 3 and sys.version_info.minor < 8:
+    from http.server import BaseHTTPRequestHandler
+    
+    # The only difference in http.server.test() between Python 3.6 and 3.7 is the default value of ServerClass
+    if sys.version_info.minor < 7: from http.server import HTTPServer as DefaultHTTPServer
+    else: from http.server import ThreadingHTTPServer as DefaultHTTPServer
+    
+    # Copy of http.server.test() from Python 3.7. ssl_wrap() call added and print statement updaed for HTTPS
+    def test(HandlerClass=BaseHTTPRequestHandler,
+             ServerClass=DefaultHTTPServer,
+             protocol="HTTP/1.0", port=8000, bind=""):
+        """Test the HTTP request handler class.
+        
+        This runs an HTTP server on port 8000 (or the port argument).
+        
+        """
+        server_address = (bind, port)
+        
+        HandlerClass.protocol_version = protocol
+        with ServerClass(server_address, HandlerClass) as httpd:
+            if args.server_certificate: httpd.socket = ssl_wrap(httpd.socket)
+            
+            sa = httpd.socket.getsockname()
+            serve_message = "Serving {proto} on {host} port {port} ({proto_lower}://{host}:{port}/) ..."
+            print(serve_message.format(host=sa[0], port=sa[1], proto=PROTOCOL, 
+                proto_lower=PROTOCOL.lower()))
+            try:
+                httpd.serve_forever()
+            except KeyboardInterrupt:
+                print("\nKeyboard interrupt received, exiting.")
+                sys.exit(0)
+else:
+    from http.server import BaseHTTPRequestHandler
+    from http.server import ThreadingHTTPServer
+    from http.server import _get_best_family
+    
+    # Copy of http.server.test() from Python 3.8. ssl_wrap() call added and print statement updaed for HTTPS
+    def test(HandlerClass=BaseHTTPRequestHandler,
+             ServerClass=ThreadingHTTPServer,
+             protocol="HTTP/1.0", port=8000, bind=None):
+        """Test the HTTP request handler class.
+        
+        This runs an HTTP server on port 8000 (or the port argument).
+        
+        """
+        ServerClass.address_family, addr = _get_best_family(bind, port)
+        
+        HandlerClass.protocol_version = protocol
+        with ServerClass(addr, HandlerClass) as httpd:
+            if args.server_certificate: httpd.socket = ssl_wrap(httpd.socket)
+            
+            host, port = httpd.socket.getsockname()[:2]
+            url_host = f'[{host}]' if ':' in host else host
+            print(
+                f"Serving {PROTOCOL} on {host} port {port} "
+                f"({PROTOCOL.lower()}://{url_host}:{port}/) ..."
+            )
+            try:
+                httpd.serve_forever()
+            except KeyboardInterrupt:
+                print("\nKeyboard interrupt received, exiting.")
+                sys.exit(0)
+
+def main():
+    global args, PROTOCOL
+    
+    # In Python 3.8, http.server.test() was altered to use None instead of '' as the default for its bind parameter
+    if sys.version_info.major == 3 and sys.version_info.minor < 8:
+        bind_default = ''
+    else:
+        bind_default = None
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', type=int, default=8000, nargs='?',
+        help='Specify alternate port [default: 8000]')
+    parser.add_argument('--cgi', action='store_true',
+        help='Run as CGI Server')
+    parser.add_argument('--bind', '-b', default=bind_default, metavar='ADDRESS',
+        help='Specify alternate bind address [default: all interfaces]')
+    parser.add_argument('--token', '-t', type=str,
+        help='Specify alternate token [default: \'\']')
+    parser.add_argument('--server-certificate', '--certificate', '-c',
+        help='Specify HTTPS server certificate to use [default: none]')
+    parser.add_argument('--client-certificate',
+        help='Specify HTTPS client certificate to accept for mutual TLS [default: none]')
+    
+    # Directory option was added to http.server in Python 3.7
+    if sys.version_info.major > 3 or sys.version_info.minor >= 7:
+        parser.add_argument('--directory', '-d', default=os.getcwd(),
+            help='Specify alternative directory [default:current directory]')
+    
+    args = parser.parse_args()
+    if not hasattr(args, 'directory'): args.directory = os.getcwd()
+    PROTOCOL = 'HTTPS' if args.server_certificate else 'HTTP' # Just for log statements
+    
+    if args.cgi:
+        handler_class = CGIHTTPRequestHandler
+    elif sys.version_info.major == 3 and sys.version_info.minor < 7:
+        handler_class = SimpleHTTPRequestHandler
+    else:
+        handler_class = functools.partial(SimpleHTTPRequestHandler, directory=args.directory)
+    
+    print('File upload available at /upload')
+    
+    # This was added to http.server's main section in Python 3.8
+    if sys.version_info.major == 3 and sys.version_info.minor < 8:
+        test(
+            HandlerClass=handler_class,
+            port=args.port,
+            bind=args.bind,
+        )
+    else:
+        class DualStackServer(http.server.ThreadingHTTPServer):
+            def server_bind(self):
+                # suppress exception when protocol is IPv4
+                with contextlib.suppress(Exception):
+                    self.socket.setsockopt(
+                        socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+                return super().server_bind()
+        
+        test(
+            HandlerClass=handler_class,
+            ServerClass=DualStackServer,
+            port=args.port,
+            bind=args.bind,
+        )
