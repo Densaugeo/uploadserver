78962cf3a9aa7fd2c196c5225b6922e90085000e
Densaugeo <>
2023-02-17 00:23:53 -0500

    v4.1.1 - One more deprecation notice

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='uploadserver',
-    version='4.1.0',
+    version='4.1.1',
     author='Densaugeo',
     author_email='author@example.com',
     description='Python\'s http.server extended to include a file upload page',
