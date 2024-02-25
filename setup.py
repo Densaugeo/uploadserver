a2fafb6e5f2fffcb4805784abd1509b6434cb20e
Densaugeo <>
2024-02-25 13:06:43 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='uploadserver',
-    version='5.1.1',
+    version='5.2.0',
     author='Densaugeo',
     author_email='author@example.com',
     description='Python\'s http.server extended to include a file upload page',
